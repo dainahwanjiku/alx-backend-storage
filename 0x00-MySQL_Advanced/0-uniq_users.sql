4a6f96e77c48ed4e2642ded67574051e31b377a8
dainahwanjiku <deeshish2017@gmail.com>
2023-01-12 13:28:08 +0000

    uniq users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,3 @@
-/*sql script that creates table users id, email, name*/
+-- sql script that creates table users id, email, name
 
 CREATE TABLE users(id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, email var(255) NOT NULL UNIQUE, name var(255));
