950355e261942cb9dcba045d63c24d34910f5462
dainahwanjiku <deeshish2017@gmail.com>
2023-01-12 15:10:27 +0000

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,5 @@
---ranks country origins of bands, ordered by the no.s of non-unique fans.
-SELECT origin AS origin, SUM(fans) AS nb_fans FROM metal_bands ORDER BY nb_fans DESC GROUP BY origin;
+-- ranks country origins of bands, ordered by the no.s of non-unique fans.
+SELECT origin AS origin, SUM(fans) AS nb_fans
+FROM metal_bands 
+GROUP BY origin
+ORDER BY nb_fans DESC;
